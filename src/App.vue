8a010f59e2c33dd4fd1e21628d4a3d2ea0e26152
NewMatheusED <matheus081105@gmail.com>
2022-07-25 20:38:38 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div class="container">
     <div class="loading hide"></div>
     <div class="main">
-      <input type="text" placeholder="Id ou nome" v-model="pokemon">
+      <input class="form-control input" type="text" placeholder="Id ou nome" v-model="pokemon">
       <img class='mainImg'/>
       <h1 style="text-transform: capitalize;">Nome: {{pokemonData['name']}}</h1>
       <h2>Id: {{pokemonData['id']}}</h2>
-      <button @click="getApiData">Search</button>
+      <button class="btn btn-primary" @click="getApiData">Search</button>
+      <button class="btn btn-secondary hide" style="margin-top: 10px" @click="__repeat">Pesquisar novamente</button>
     </div><!--main-->
   </div><!--container-->
 </template>
@@ -26,12 +27,23 @@
           const div1 = document.querySelector('.loading');
           const div2 = document.querySelector('.main');
           const img = document.querySelector('.mainImg');
+          const input = document.querySelector('.input');
+          const btn1 = document.querySelector('.btn-primary');
+          const btn2 = document.querySelector('.btn-secondary');
           this.pokemonData = [];
           div1.classList.add("show");
           div1.classList.remove("hide");
           div2.classList.add("hide");
           div2.classList.remove("show");
+          btn1.classList.add("hide");
+          btn1.classList.remove("show");
+          btn2.classList.add("show");
+          btn2.classList.remove("hide");
           try {
+            if(this.pokemon === '') {
+              alert('Preencher campo')
+              window.location.reload();
+            }
             let name = this.pokemon.toLowerCase();
             const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
             if(res.status == 200) { //se o pokemon existe
@@ -39,6 +51,7 @@
               this.pokemonData = data ;
               this.pokemon = '';
               img.src = data['sprites']['versions']['generation-v']['black-white']['front_default'];
+              input.classList.add("hide");
               if (this.pokemonData.length != 0) { 
                 div1.classList.add("hide");
                 div1.classList.remove("show");
@@ -48,6 +61,7 @@
             } else if (res.status == 404) { //se o pokemon não existe
               this.pokemonData.name = 'Pokemon não encontrado';
               this.pokemonData.id = 'Error 404';
+              img.src = '';
               div1.classList.add("hide");
               div1.classList.remove("show");
               div2.classList.add("show");
@@ -59,6 +73,10 @@
           catch(error) {
             console.log(error);
           }
+        },
+
+        __repeat() {
+          window.location.reload();
         }
       },
 
@@ -91,12 +109,18 @@
     animation: spin 1s linear infinite;
    }
 
+   .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+   }
+
    @keyframes spin {
     0% {
-      transform: rotate(0deg);
+      transform:translate(-50%, -50%) rotate(0deg); 
     }
     100% {
-      transform: rotate(360deg);
+      transform: translate(-50%, -50%) rotate(360deg) ;
     }
    }
 </style>
