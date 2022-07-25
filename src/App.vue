15202d852fd000d742b952b26962eadc9ed8d435
NewMatheusED <matheus081105@gmail.com>
2022-07-25 12:19:54 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>{{message}}</h1>
+  <div v-bind:class = "(this.pokemonData.length == 0) ? 'show' : 'hide'" class="loading"></div>
+  <input type="text" placeholder="Id ou nome" v-model="pokemon">
+  <h1>Nome: {{pokemonData.name}}</h1>
+  <h2>Id: {{pokemonData.id}}</h2>
+  <button @click="getApiData">Get Data</button>
 </template>
 
 <script>
@@ -7,14 +11,67 @@
     name: "App",
     data() {
       return {
-        message: "Hello Everyone!"
+        pokemonData: [],
+        pokemon: ''
+        }
+      },  
+
+      methods: {
+        async getApiData() {
+          try {
+            if(this.pokemonData.length == 0) {
+              
+            }
+            let name = this.pokemon.toLowerCase();
+            this.pokemonData = [];
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+            const data = await res.json();
+            this.pokemonData = data ;
+            this.pokemon = '';
+
+          }
+          catch(error) {
+            console.log(error)
+          }
+        }
+      },
+
+      creates() {
+        this.getApiData()
       }
-    }
   }
 </script>
 
 <style>
-  h1 {
-    color: red;
-  }
+   .show {
+    display: inline-block;
+   }
+
+   .hide {
+    display: none;
+   }
+
+   .loading {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: #fff;
+    border: 2px solid #000;
+    border-left-color: #fff;
+    animation: spin 1s linear infinite;
+   }
+
+   @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+   }
 </style>
