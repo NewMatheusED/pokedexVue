603b965b2f47734121472769055d1b8bebf72248
NewMatheusED <matheus081105@gmail.com>
2022-07-25 20:00:23 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     <div class="loading hide"></div>
     <div class="main">
       <input type="text" placeholder="Id ou nome" v-model="pokemon">
-      <h1>Nome: {{pokemonData.name}}</h1>
-      <h2>Id: {{pokemonData.id}}</h2>
-      <button @click="getApiData">Get Data</button>
+      <img class='mainImg'/>
+      <h1 style="text-transform: capitalize;">Nome: {{pokemonData['name']}}</h1>
+      <h2>Id: {{pokemonData['id']}}</h2>
+      <button @click="getApiData">Search</button>
     </div><!--main-->
   </div><!--container-->
 </template>
@@ -24,27 +25,27 @@
         async getApiData() {
           const div1 = document.querySelector('.loading');
           const div2 = document.querySelector('.main');
+          const img = document.querySelector('.mainImg');
           this.pokemonData = [];
+          div1.classList.add("show");
+          div1.classList.remove("hide");
+          div2.classList.add("hide");
+          div2.classList.remove("show");
           try {
-            if(this.pokemonData.length == 0) {
-              div1.classList.add("show");
-              div1.classList.remove("hide");
-              div2.classList.add("hide");
-              div2.classList.remove("show");
-            }
             let name = this.pokemon.toLowerCase();
             const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-            if(res.status == 200) {
+            if(res.status == 200) { //se o pokemon existe
               const data = await res.json();
               this.pokemonData = data ;
               this.pokemon = '';
-              if (this.pokemonData.length != 0) {
+              img.src = data['sprites']['versions']['generation-v']['black-white']['front_default'];
+              if (this.pokemonData.length != 0) { 
                 div1.classList.add("hide");
                 div1.classList.remove("show");
                 div2.classList.add("show");
                 div2.classList.remove("hide");
               }
-            } else if (res.status == 404) {
+            } else if (res.status == 404) { //se o pokemon não existe
               this.pokemonData.name = 'Pokemon não encontrado';
               this.pokemonData.id = 'Error 404';
               div1.classList.add("hide");
